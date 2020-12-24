88841ca462a29cc3b0718487df85fb37740b207f
arnold <arnoldnderitu@gmail.com>
2020-12-24 22:47:36 +0300

    Backend files

---
+++ back-end/app/crm/routes.py
@@ -0,0 +1,10 @@
+from flask import render_template, redirect, url_for, flash, request
+from werkzeug.urls import url_parse
+from flask_babel import _
+from app import db
+from app.crm import bp
+
+
+@bp.route('/kanban', methods=['GET', 'POST'])
+def crm():
+    return render_template('crm/kanban.html', title=_('CRM | Kanban Boards'))
